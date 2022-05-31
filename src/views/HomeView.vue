7ddfe85340dc4fd5ad5078960c6612623c2b02a6
katan514 <m5014514m@gmail.com>
2022-06-01 00:43:46 +0900

    vue3.2コードに書き直し。 ・defineConfig廃止。 ・sctipt setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,7 @@
   <hello-world />
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-
+<script setup lang="ts">
 // Components
 import HelloWorld from "../components/HelloWorld.vue";
-
-export default defineComponent({
-  name: "HomeView",
-
-  components: {
-    HelloWorld,
-  },
-});
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
 const { defineConfig } = require("@vue/cli-service");
+// /import path from "path";
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  // configureWebpack: {
+  //   resolve: {
+  //     alias: {
+  //       "@": path.join(__dirname, "/src/componentes"), // 1. @の参照先の変更
+  //     },
+  //     extensions: [".js", ".vue", ".json", ".ts"],
+  //   },
+  // },
+  // outputDir: "src/dist/resources/static", // 2. 出力先
+  // pages: {
+  //   index: {
+  //     entry: "src/main.ts", // エントリーポイント
+  //     template: "src/index.html", //3. index.htmlテンプレート
+  //     filename: "index.html", // 省略可
+  //   },
+  // },
 });
